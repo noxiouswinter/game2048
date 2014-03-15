08e9e8079b64ebe4ee0dbb9f871b8a594e58c9b8
jpkader <jpkader@amplify.com>
2014-03-15 00:20:59 -0400

    Adding jUnit dummy test class for the android project.

---
+++ Android/tests/com/jinais/game2048/GameTests.java
@@ -0,0 +1,33 @@
+package com.jinais.game2048;
+
+import junit.framework.TestCase;
+
+public class GameTests extends TestCase {
+
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	protected static void tearDownAfterClass() throws Exception {
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testPlayMatrix() {
+		fail("Not yet implemented");
+	}
+
+	public void testResetGame() {
+		fail("Not yet implemented");
+	}
+
+	public void testCheckIfGameOver() {
+		fail("Not yet implemented");
+	}
+
+}
