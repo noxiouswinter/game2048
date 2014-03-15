702a0b118104745c5be1fc88cd9b6d4cc964bfe5
jpkader <jpkader@amplify.com>
2014-03-14 20:18:50 -0400

    Adding 2048Logic and gitignore

---
+++ 2048logic/test/game/GameTests.java
@@ -0,0 +1,14 @@
+package game;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class GameTests {
+	
+	@Test
+	public void testSanity(){
+		Assert.assertEquals(true, true);
+	}
+
+}
